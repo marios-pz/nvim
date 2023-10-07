71642ab4fa0712ff8019e5582fb45c23071783e6
Mario <30930688+mario-pz@users.noreply.github.com>
2023-10-07 23:56:25 +0300

    Demo requirements (#2)
    * removed more plugins I decided not to continue with and optimized lazy
    * Add dockerfile for demos

--- lua/convenience/init.lua
+++ lua/convenience/init.lua
@@ -1,6 +1,5 @@
 -- Convenience contains optional components that bring an easier experience
 
 require("convenience.bufferline")
-require("convenience.neotest")
 require("convenience.trouble")
 require("convenience.vim-surround")
