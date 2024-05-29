a8ea847a083ed67938ed7752d14b13d5708d7bb1
marios-pz <marios-pz@proton.me>
2024-05-29 17:00:14 +0300

    disable colourscheme, update dependencies

--- lua/marios/plugins/colorscheme.lua
+++ lua/marios/plugins/colorscheme.lua
@@ -1,21 +1,13 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            require("tokyonight").setup({
-                style = "storm",
-            })
-            -- load the colorscheme here
-            vim.cmd([[colorscheme tokyonight-storm]])
-        end,
-    },
-    {
-        "uniwa-community/uniwa.nvim",
-        priority = 1000, -- make sure to load this before all the other start plugins
-        dependencies = {
-            "rktjmp/lush.nvim",
-            version = false, -- testing with last commit
-        },
-    },
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("tokyonight").setup({
+				style = "storm",
+			})
+			-- load the colorscheme here
+			--vim.cmd([[colorscheme tokyonight-storm]])
+		end,
+	},
 }
--- lua/marios/plugins/lsp/mason.lua
+++ lua/marios/plugins/lsp/mason.lua
@@ -1,117 +1,117 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = function()
-        -- import mason
-        local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-        -- import mason-lspconfig
-        local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-        local mason_tool_installer = require("mason-tool-installer")
+		local mason_tool_installer = require("mason-tool-installer")
 
-        -- enable mason and configure icons
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-        })
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-        mason_lspconfig.setup({
-            ensure_installed = {
-                -- CSS
-                "cssls",
-                "cssmodules_ls",
-                "unocss",
-                "als",
-                "tailwindcss",
-                -- Languages
-                "clangd",
-                "cmake",
-                "lua_ls",
-                "html",
-                "jedi_language_server",
-                "tsserver",
-                "rust_analyzer",
-                "sqlls",
-                "jsonls",
-                "vimls",
-                "golangci_lint_ls",
-                "gopls",
-                "emmet_ls",
-                -- "arduino_language_server",
-                -- "asm_lsp",
-                "typst_lsp",
+		mason_lspconfig.setup({
+			ensure_installed = {
+				-- CSS
+				"cssls",
+				"cssmodules_ls",
+				"unocss",
+				"als",
+				"tailwindcss",
+				-- Languages
+				"clangd",
+				"cmake",
+				"lua_ls",
+				"html",
+				"jedi_language_server",
+				"tsserver",
+				"rust_analyzer",
+				"sqlls",
+				"jsonls",
+				"vimls",
+				"golangci_lint_ls",
+				"gopls",
+				"emmet_ls",
+				-- "arduino_language_server",
+				-- "asm_lsp",
+				"typst_lsp",
 
-                -- DevOps
-                "yamlls",
-                "ansiblels",
-                "azure_pipelines_ls",
-                "bashls",
-                "dockerls",
-                "docker_compose_language_service",
-                "gradle_ls",
-                "graphql",
-                "helm_ls",
-                "jsonnet_ls",
-                "marksman",
-                "terraformls",
-                -- "circleci-yaml-language-server",
-                "gitlab_ci_ls",
-            },
-            -- auto-install configured servers (with lspconfig)
-            automatic_installation = true, -- not the same as ensure_installed
-        })
+				-- DevOps
+				"yamlls",
+				"ansiblels",
+				"azure_pipelines_ls",
+				"bashls",
+				"dockerls",
+				"docker_compose_language_service",
+				"gradle_ls",
+				"graphql",
+				"helm_ls",
+				"jsonnet_ls",
+				"marksman",
+				"terraformls",
+				-- "circleci-yaml-language-server",
+				-- "gitlab_ci_ls",
+			},
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
 
-        mason_tool_installer.setup({
-            ensure_installed = {
-                -- BASE
-                "stylua",
-                "luacheck",
+		mason_tool_installer.setup({
+			ensure_installed = {
+				-- BASE
+				"stylua",
+				"luacheck",
 
-                -- Shell
-                "beautysh",
-                "shellcheck",
-                "bash-debug-adapter",
+				-- Shell
+				"beautysh",
+				"shellcheck",
+				"bash-debug-adapter",
 
-                -- C++
-                "clang-format",
-                "cpplint",
-                "cpptools",
+				-- C++
+				"clang-format",
+				"cpplint",
+				"cpptools",
 
-                -- Python
-                "black", -- python formatter
-                "isort", -- format python imports
-                "debugpy",
+				-- Python
+				"black", -- python formatter
+				"isort", -- format python imports
+				"debugpy",
 
-                -- Golang
-                "go-debug-adapter",
+				-- Golang
+				"go-debug-adapter",
 
-                -- WEB
-                "htmlhint",
-                "prettier",
-                "eslint_d", -- js linter
+				-- WEB
+				"htmlhint",
+				"prettier",
+				"eslint_d", -- js linter
 
-                -- Other
-                "typstfmt",
-                "checkstyle",
+				-- Other
+				"typstfmt",
+				"checkstyle",
 
-                -- DevOps
-                "actionlint",
-                "ansible-lint",
-                "jsonlint",
-                "yamllint",
-                "yamlfmt",
-                "hadolint",
-                "tflint",
-            },
-        })
-    end,
+				-- DevOps
+				"actionlint",
+				"ansible-lint",
+				"jsonlint",
+				"yamllint",
+				"yamlfmt",
+				"hadolint",
+				"tflint",
+			},
+		})
+	end,
 }
