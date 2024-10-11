38e600c309512ed08714b88672d598052dbb580c
marios-pz <marios-pz@proton.me>
2024-10-12 00:28:21 +0300

    fix blink.cmp shit ctrl-j ctrl-k instead of n,p+y

--- lua/marios/plugins/lsp/blink.lua
+++ lua/marios/plugins/lsp/blink.lua
@@ -30,5 +30,21 @@ return {
 
 		-- experimental signature help support
 		trigger = { signature_help = { enabled = true } },
+
+		keymap = {
+			show = "<C-space>",
+			hide = "<C-e>",
+			accept = "<C-y>",
+			select_prev = { "<Up>", "<C-p>" },
+			select_next = { "<Down>", "<C-n>" },
+
+			show_documentation = "<C-space>",
+			hide_documentation = "<C-space>",
+			scroll_documentation_up = "<C-b>",
+			scroll_documentation_down = "<C-f>",
+
+			snippet_forward = "<Tab>",
+			snippet_backward = "<S-Tab>",
+		},
 	},
 }
