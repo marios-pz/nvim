03f3f7a2e3d8da9c44eb40ac4f5a8f65ee5311a2
marios-pz <marios-pz@proton.me>
2024-11-13 18:23:06 +0200

    Added gotham-theme, yaml plugin for easier debugging and synced lazy

--- lua/marios/plugins/colorscheme.lua
+++ lua/marios/plugins/colorscheme.lua
@@ -1,13 +1,20 @@
 return {
 	{
-		"folke/tokyonight.nvim",
+		"whatyouhide/vim-gotham",
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			require("tokyonight").setup({
-				style = "storm",
-			})
-			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight-storm]])
+			vim.cmd([[colorscheme gotham]])
 		end,
 	},
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	priority = 1000, -- make sure to load this before all the other start plugins
+	-- 	config = function()
+	-- 		require("tokyonight").setup({
+	-- 			style = "storm",
+	-- 		})
+	-- 		-- load the colorscheme here
+	-- 		vim.cmd([[colorscheme tokyonight-storm]])
+	-- 	end,
+	-- },
 }
