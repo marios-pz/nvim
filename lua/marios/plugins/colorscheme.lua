f0289c0905e18fb080b769c20a0dbbc4b55f159f
marios-pz <marios-pz@proton.me>
2025-03-31 11:39:26 +0300

    tokyonight back baby

--- lua/marios/plugins/colorscheme.lua
+++ lua/marios/plugins/colorscheme.lua
@@ -1,20 +1,20 @@
 return {
-	{
-		"whatyouhide/vim-gotham",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			vim.cmd([[colorscheme gotham]])
-		end,
-	},
 	-- {
-	-- 	"folke/tokyonight.nvim",
+	-- 	"whatyouhide/vim-gotham",
 	-- 	priority = 1000, -- make sure to load this before all the other start plugins
 	-- 	config = function()
-	-- 		require("tokyonight").setup({
-	-- 			style = "storm",
-	-- 		})
-	-- 		-- load the colorscheme here
-	-- 		vim.cmd([[colorscheme tokyonight-storm]])
+	-- 		vim.cmd([[colorscheme gotham]])
 	-- 	end,
 	-- },
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("tokyonight").setup({
+				style = "storm",
+			})
+			-- load the colorscheme here
+			vim.cmd([[colorscheme tokyonight-storm]])
+		end,
+	},
 }
